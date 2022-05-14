4156271c37be2a483312eca9af2319d52f4c258b
Emmanuel <emmanuelappiah124@gmail.com>
2022-05-14 06:17:37 +0100

    Implemented states into the registration form. Yet to add combine the registration service to it.

--- stocXtune/frontend/src/pages/RegisterPage.js
+++ stocXtune/frontend/src/pages/RegisterPage.js
@@ -1,9 +1,58 @@
+import { useState } from "react";
 import { backendBaseUrl } from "../core/constants";
 import useFetch from "../services/useFetch"
 
 export default function RegisterPage() {
-    const {data, loading,error} = useFetch(backendBaseUrl,null,'/user/register');
+    // States for trackin user registration form fields
+    const [firstName, setFirstName] =useState('')
+    const [lastName, setLastName] =useState('')
+    const [userName, setUserName] =useState('')
+    const [email, setEmail] =useState('')
+    const [password, setPassword] =useState('')
+    const [confirmPassword, setConfirmPassword] =useState('')
 
+    
+
+    // States for checking errors
+    const [submitted, setSubmitted] = useState(false)
+    const [error, setError] = useState(false)
+
+    
+
+    // Handle on FirstNName change event
+    const handleInput = (event) => {
+        console.log(event.target.id)
+       
+        
+
+        switch (event.target.id) {
+            case 'firstName':
+                console.log('setting firstName')
+                setFirstName(event.target.value)
+                break;
+            case 'lastName':
+                console.log('setting lastname')
+                setLastName(event.target.value)
+                break;
+
+            case 'email':
+                console.log('setting email')
+                setEmail(event.target.value)
+                break;
+
+            case 'password':
+                console.log('setting password')
+                setPassword(event.target.value)
+                break;
+            case 'confirmPassword':
+                console.log('setting confirm password')
+                setConfirmPassword(event.target.value)
+                break;
+            default:
+                break;
+        }
+        setSubmitted(false);
+    }
 
     return (
         <section className="pt-32 bg-cover bg-[url('../public/assets/register_bg.png')] min-h-screen">
@@ -18,35 +67,35 @@ export default function RegisterPage() {
                     <div className="grid grid-cols-2 gap-8">
                        <div>
                            <label>First Name</label>
-                           <input
+                           <input name="firstName" id="firstName" value={firstName} onChange={handleInput}
                                className=' mr-2 h-14 uppercase text-md bg-lighter-teal/20 input w-full placeholder:capitalize placeholder:text-neutral/50'
                                placeholder='First Name' type="text" aria-autocomplete="list"/>
                        </div>
                         <div>
                             <label>Last Name</label>
-                            <input
+                            <input name="lastName" id="lastName" value={lastName} onChange={handleInput}
                                 className=' mr-2 h-14 uppercase text-md bg-lighter-teal/20 input w-full placeholder:capitalize placeholder:text-neutral/50'
                                 placeholder='Last Name' type="text" aria-autocomplete="list"/>
                         </div>
                         <div className="col-span-2">
                             <label>Email Address</label>
-                            <input
+                            <input name="email" id="email" value={email} onChange={handleInput}
                                 className=' mr-2 h-14 uppercase text-md bg-lighter-teal/20 input w-full placeholder:capitalize placeholder:text-neutral/50'
                                 placeholder='Email Address' type="email" aria-autocomplete="list"/>
                         </div>
                         <div className="col-span-2">
                             <label>Password</label>
-                            <input
+                            <input name="password" id="password" value={password} onChange={handleInput}
                                 className=' mr-2 h-14 uppercase text-md bg-lighter-teal/20 input w-full placeholder:capitalize placeholder:text-neutral/50'
                                 placeholder='Enter password' type="password" aria-autocomplete="list"/>
                         </div>
                         <div className="col-span-2">
                             <label>Re-enter Password</label>
-                            <input
+                            <input name="confirmPassword" id="confirmPassword" value={confirmPassword} onChange={handleInput}
                                 className=' mr-2 h-14 uppercase text-md bg-lighter-teal/20 input w-full placeholder:capitalize placeholder:text-neutral/50'
                                 placeholder='Confirm your password' type="password" aria-autocomplete="list"/>
                         </div>
-                        <button className="btn btn-primary col-span-2 btn-lg">Create an Account</button>
+                        <button  className="btn btn-primary col-span-2 btn-lg">Create an Account</button>
                     </div>
                 </div>
                 <div className="col-span-3"></div>
